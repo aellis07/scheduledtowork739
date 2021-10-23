21bc32fc4c824c306aec41602928adc9e4b36e5d
Anthony Ellis <amellis245@gmail.com>
2021-10-22 23:27:25 -0400

    added conditions for timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,25 @@
 
 var currentDateDisplay = $("#currentDay");
 var today = moment();
+var currentTime = moment().format("LT");
 
-windowInit();
+// FUNCTION
 
-function windowInit() {
+function pageInit() {
   currentDateDisplay.text(today.format("LL"));
+  findCurrentTime();
+}
+
+pageInit();
+
+function findCurrentTime() {
+  //reads current hour and compares to equivalent hour value, changes class for CSS styling
+  if (currentTime < 7) {
+    $("#a7").addClass("future");
+  } else if (currentTime == 7) {
+    $("#a7").addClass("present");
+  } else {
+    $("#a7").addClass("past");
+  }
+  console.log(currentTime);
 }
